5862354d632783b30f0825c093f5f55597a3b486
zhangbao <zhangbao90s@qq.com>
2021-04-25 23:32:58 +0800

    feat: add init files

---
+++ index.js
@@ -0,0 +1,36 @@
+let Parser = require('rss-parser');
+let parser = new Parser({
+    // 30 秒后取消请求
+    timeout: 30000
+});
+const fs = require('fs/promises');
+const process = require('process');
+
+
+async function main() {
+    const url = 'https://cprss.s3.amazonaws.com/javascriptweekly.com.xml';
+
+    let feed = await parser.parseURL(url);
+
+    console.log(`successfully generating new feed.`);
+
+    await fs.rmdir('./dist', { recursive: true });
+    console.log(`successfully deleted ./dist`);
+
+    await fs.mkdir('./dist');
+    console.log(`successfully create ./dist`);
+
+    await fs.writeFile('./dist/rss.json', JSON.stringify(feed));
+    console.log(`successfully write rss.json`);
+
+    await fs.copyFile('./template/index.html', `./dist/index.html`);
+    await fs.copyFile('./template/page.js', `./dist/page.js`);
+    console.log(`successfully copy asset files`);
+
+}
+
+main()
+.catch(err => {
+  console.log(err);
+  process.exit(1);
+});
