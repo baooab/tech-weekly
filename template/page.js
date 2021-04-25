79f1e23ed102915212d55204b698363c7d3125e7
zhangbao <zhangbao90s@qq.com>
2021-04-25 23:53:48 +0800

    chore: fix template code

--- template/page.js
+++ template/page.js
@@ -1,19 +1,26 @@
 fetch('./rss.json')
-.then(async function (response) {
-  const res = await response.json();
-  const items = res.items;
+  .then(async function (response) {
+    const res = await response.json();
+    const items = res.items;
 
-  const list = document.querySelector('.list');
-  const fragment = document.createDocumentFragment();
+    const issues = document.querySelector('.issues');
+    const fragment = document.createDocumentFragment();
 
-  items.forEach(i => {
-    const li = document.createElement('div');
-    const p = document.createElement('p');
-    const timeObj = new Date(i.pubDate);
-    p.innerHTML = `${i.title} （<a href="${i.link}" target="_blank">${timeObj.toLocaleTimeString('zh-CN', {hour12: false, hour: '2-digit', minute: '2-digit'})}</a>）`;
-    li.appendChild(p);
-    fragment.appendChild(li);
-  });
+    items.forEach(i => {
+      const issue = document.createElement('details');
+      const h2 = document.createElement('summary');
+      h2.classList.add('title', 'is-4');
+      const content = document.createElement('div');
 
-  list.appendChild(fragment);
-})
+      const timeObj = new Date(i.pubDate);
+      const weekDays = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+      h2.innerHTML = `${i.title} （<a href="${i.link}" target="_blank">${timeObj.toLocaleDateString()} ${weekDays[timeObj.getDay()]}</a>）`;
+      content.innerHTML = i.content;
+
+      issue.appendChild(h2);
+      issue.appendChild(content);
+      fragment.appendChild(issue);
+    });
+
+    issues.appendChild(fragment);
+  })
