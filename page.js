28795f8e71e8a42fd5b3f8f2a5b3003dbe7e65c6
baooab <zhangbao90s@qq.com>
2021-04-29 00:09:39 +0000

    Deploying to gh-pages from  @ f6527e1ad2030536a62a865ec4fa0400f83758f4 🚀

--- page.js
+++ page.js
@@ -1,26 +1,43 @@
-fetch('./rss.json')
-  .then(async function (response) {
-    const res = await response.json();
-    const items = res.items;
 
-    const issues = document.querySelector('.issues');
-    const fragment = document.createDocumentFragment();
+  function fetchWeekly(url, selector) {
+    fetch(url)
+    .then(async function (response) {
+      const res = await response.json();
+      const items = res.items;
+  
+      const issues = document.querySelector(selector);
+      const fragment = document.createDocumentFragment();
+  
+      const title = document.createElement('div');
+      title.innerText = res.title
+      title.classList.add('is-6', 'title')
+      // const description = document.createElement('small');
+      // description.innerText = res.description;
+      // description.classList.add('ml-3')
+      // title.appendChild(description)
 
-    items.forEach(i => {
-      const issue = document.createElement('details');
-      const h2 = document.createElement('summary');
-      h2.classList.add('title', 'is-4');
-      const content = document.createElement('div');
+      fragment.appendChild(title)
 
-      const timeObj = new Date(i.pubDate);
-      const weekDays = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
-      h2.innerHTML = `${i.title} （<a href="${i.link}" target="_blank">${timeObj.toLocaleDateString()} ${weekDays[timeObj.getDay()]}</a>）`;
-      content.innerHTML = i.content;
+      items.forEach(i => {
+        const issue = document.createElement('details');
+        const h2 = document.createElement('summary');
+        const content = document.createElement('div');
+        content.classList.add('issue-content')
+        const timeObj = new Date(i.pubDate);
+        const weekDays = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
+        h2.innerHTML = `${i.title} （<a href="${i.link}" target="_blank">${timeObj.toLocaleDateString()} ${weekDays[timeObj.getDay()]}</a>）`;
+        content.innerHTML = i.content;
+  
+        issue.appendChild(h2);
+        issue.appendChild(content);
+        fragment.appendChild(issue);
+      });
+  
+      issues.appendChild(fragment);
+    })
+  
+  }
 
-      issue.appendChild(h2);
-      issue.appendChild(content);
-      fragment.appendChild(issue);
-    });
 
-    issues.appendChild(fragment);
-  })
+fetchWeekly('./js.json', '.issues-js')
+fetchWeekly('./fe.json', '.issues-fe')
